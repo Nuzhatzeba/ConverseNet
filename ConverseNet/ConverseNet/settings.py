f0c65e4741d532f4e90c068938ca8829eaa981f8
“Rabeya <rabeyarupa7815@gmail.com>
2023-08-26 00:31:33 +0600

    Completed login.html

--- ConverseNet/ConverseNet/settings.py
+++ ConverseNet/ConverseNet/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+os.path.join(BASE_DIR, 'Home','templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = "ConverseNet.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'Home', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -112,6 +112,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'Home/static'),]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -122,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
