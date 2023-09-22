e8da23f307caad88d4c9c73f1b43506b6afb9a29
Prokriti Hasan Rodushi <hprokriti670@gmail.com>
2023-09-22 02:52:43 -0700

    loadpage updated

--- ConverseNet/Home/views.py
+++ ConverseNet/Home/views.py
@@ -5,16 +5,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
 # Create your views here.
-<<<<<<< HEAD
 
-=======
 from .models import ConverseNetUser, Profile, Bot_Message, FriendsThread, Requests, FriendsThreadMessage, Diary
-from .forms import ClientForm
+# from .forms import ClientForm
 
 
 def index(request):
     return render(request, "Home/index.html", {})
->>>>>>> d069773d0c22ea8fe426e68543e9fb3792f232e8
+
 def login(request):
     return render(request, 'Home/login.html',{})
 def signin(request):
