b2305cbbfbf62a4027ba33fb2a2a009049d8e42d
Prokriti Hasan Rodushi <hprokriti670@gmail.com>
2023-10-15 03:04:32 -0700

    adding friends with friend lists

--- ConverseNet/Home/views.py
+++ ConverseNet/Home/views.py
@@ -428,3 +428,46 @@ def password_reset(request, user_name):
             messages.error(request, 'WRONG USER PASSWORD ENTERED!!!!')
 
     return render(request, 'Home/updatepassword.html', {'user_name': username})
+
+
+
+def addfriend_page(request, user_name):
+    if User.objects.filter(username=user_name).exists():
+        user = User.objects.get(username=user_name)
+        user_email = user.email
+        if request.method == 'POST':
+            friend_email = request.POST['friend_email']
+            value = 'T'
+            user = ConverseNetUser.objects.all()
+            if user_email != friend_email:
+                if user.filter(email=user_email).exists() and user.filter(email=friend_email).exists():
+                    us1 = ConverseNetUser.objects.get(email=user_email)
+                    us2 = ConverseNetUser.objects.get(email=friend_email)
+                    fi1 = FriendsThread.objects.filter(friends_User_id_Person1=us1)
+                    for fi in fi1:
+                        if fi.friends_User_id_Person2 == us2:
+                            value = 'f'
+                            break
+                    fi1 = FriendsThread.objects.filter(friends_User_id_Person2=us1)
+                    for fi in fi1:
+                        if fi.friends_User_id_Person1 == us2:
+                            value = 'f'
+                            break
+                    if value == 'T':
+                        u1 = FriendsThread(friends_User_id_Person1=us1, friends_User_id_Person2=us2)
+                        u1.save()
+                        # Get the list of friends for the user
+                        friends_list = FriendsThread.objects.filter(friends_User_id_Person1=us1)
+                        return render(request, 'Home/addfriend.html', {'user_name': user_name, 'friends_list': friends_list})
+                    else:
+                        messages.error(request, 'ALREADY A FRIEND !! ')
+                else:
+                    messages.error(request, 'THE PERSON DO NOT EXIST!')
+            else:
+                messages.error(request, 'INVALID INPUT!')
+        else:
+            # Get the list of friends for the user
+            us1 = ConverseNetUser.objects.get(email=user_email)
+            friends_list = FriendsThread.objects.filter(friends_User_id_Person1=us1)
+            return render(request, 'Home/addfriend.html', {'user_name': user_name, 'friends_list': friends_list})
+
